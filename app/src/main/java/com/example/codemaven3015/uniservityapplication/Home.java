3ff3cd288b58acd06f2b012a21ee71f159b75858
nehaTom <neha.tomar@codemaven.in>
2018-05-08 18:36:56 +0530

    ....

--- app/src/main/java/com/example/codemaven3015/uniservityapplication/Home.java
+++ app/src/main/java/com/example/codemaven3015/uniservityapplication/Home.java
@@ -1,10 +1,14 @@
 package com.example.codemaven3015.uniservityapplication;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -98,6 +102,7 @@ LinearLayout UKRAINE_textview,RUSSIA_textview;
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -115,10 +120,50 @@ LinearLayout UKRAINE_textview,RUSSIA_textview;
             Intent intent= new Intent(Home.this,Colleges.class);
             intent.putExtra("menu","Universities");
             startActivity(intent);
+        }else if (id == R.id.menu_callUs){
+            askPermissionForCall();
+        }else if (id == R.id.menu_emailUs){
+            askPermissionForMail();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void askPermissionForMail(){
+        Contact_Us contact_us = new Contact_Us(this);
+        contact_us.emailNow("harpreet241608@gmail.com");
+    }
+
+
+    //call permission check
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void askPermissionForCall(){
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    android.Manifest.permission.CALL_PHONE
+            }, 8);
+            return;
+        }else {
+            Contact_Us contact_us = new Contact_Us(this);
+            contact_us.callNow("8882001245");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 8:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //startActivity(makeCall);
+                    Contact_Us contact_us = new Contact_Us(this);
+                    contact_us.callNow("8882001245");
+                }
+
+
+        }
     }
 }
