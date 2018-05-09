35c89e5db4424fcd22c385d4a0e1b8be56bf46bf
nehaTom <neha.tomar@codemaven.in>
2018-05-09 11:34:15 +0530

    ....

--- app/src/main/java/com/example/codemaven3015/uniservityapplication/Login.java
+++ app/src/main/java/com/example/codemaven3015/uniservityapplication/Login.java
@@ -1,7 +1,9 @@
 package com.example.codemaven3015.uniservityapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,7 +19,20 @@ public class Login extends AppCompatActivity {
         Mobile_editText=findViewById(R.id.Mobile_editText);
         Mail_editText=findViewById(R.id.Mail_editText);
         cancelBtn=findViewById(R.id.cancelBtn);
+        setsubmitBtn();
+
+    }
+
+    private void setsubmitBtn()
+    {
         submitBtn=findViewById(R.id.submitBtn);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Login.this,Otp.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
