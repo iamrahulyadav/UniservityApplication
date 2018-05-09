5304e5faff90874edd81dc23628b7b0085bd925c
nehaTom <neha.tomar@codemaven.in>
2018-05-09 12:56:59 +0530

    first Commit

--- app/src/main/java/com/example/codemaven3015/uniservityapplication/Otp.java
+++ app/src/main/java/com/example/codemaven3015/uniservityapplication/Otp.java
@@ -1,13 +1,29 @@
 package com.example.codemaven3015.uniservityapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Otp extends AppCompatActivity {
+    Button submitOtp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        setOnClickOtpButton();
+    }
+
+    private void setOnClickOtpButton() {
+        submitOtp = findViewById(R.id.submitOtp);
+        submitOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Otp.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
